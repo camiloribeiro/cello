9c4a48c538ee8bb2d7e69814607bc2de44dd5d77
cribeiro <Thoughtworks@TW008317.local>
2012-05-27 17:50:10 -0300

    Adding dependences reference

--- lib/cello.rb
+++ lib/cello.rb
@@ -1,1 +1,11 @@
-a
+require 'cello/version'
+require 'rubygems'
+require 'bundler/setup'
+
+#common dependencies
+require 'selenium-webdriver'
+require 'watir-webdriver'
+require 'cucumber'
+
+module Cello
+end
