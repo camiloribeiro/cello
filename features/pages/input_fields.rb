c5c806ddd1fdc7b5ee3dd816c34ee17680c273f8
cribeiro <Thoughtworks@tw008317.home>
2012-05-29 00:47:57 -0300

    adding feature to basic access methods and fixing feature description

--- features/pages/input_fields.rb
+++ features/pages/input_fields.rb
@@ -4,6 +4,8 @@ module StaticPages
   module Site
     class InputPage < Cello::Structure::Page
       element :text_field,  :text_field, :id => 'text1'
+      element :text_fieldname,  :text_field, :name => 'text1'
+      element :text_fieldxpath,  :text_field, :xpah, '//*[@id="text1"]'
       element :checkbox,  :checkbox, :id => 'check1'
 
       @@url = 'file://' + File.dirname(__FILE__) + '/../site/inputs.html'
--- features/step_definitions/access_element.rb
+++ features/step_definitions/access_element.rb
@@ -3,20 +3,22 @@ Dir[File.dirname(__FILE__) + "/../../pages/*.rb"].each do |file|
 end
 
 Then /^I should be able to write "(.*?)" in a element using id$/ do |text|
-
+  @page.text_field_fill_with(text)
   @page.close
 end
 
 Then /^I should be able to write "(.*?)" in a element using name$/ do |text|
+  @page.text_fieldname_fill_with(text)
   @page.close
 end
 
 Then /^I should be able to write "(.*?)" in a element using label$/ do |text|
-  @page.close
+  #pending
 end
 
 Then /^I should be able to write "(.*?)" in a element using xpath$/ do |text|
-  @page.close
+  #pending
+  @page.text_fieldxpath_fill_with(text)
 end
 
 
