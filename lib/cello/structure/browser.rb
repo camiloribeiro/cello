17edd3aa6b01f234b9c16d1eeae58f51b69f7325
cribeiro <Thoughtworks@tw008317.home>
2012-06-29 02:42:29 -0300

    creating browser to provide context

---
+++ lib/cello/structure/browser.rb
@@ -0,0 +1,51 @@
+require "rubygems"
+require "watir-webdriver"
+require "headless"
+
+module Cello
+  module Structure
+    class Browser
+
+      def initialize(browser)
+        @headless = Headless.new
+        @headless.start
+        @browser = Watir::Browser.new browser
+      end
+      
+      attr_reader :browser   
+      def visit
+        @browser.goto @context.get_url
+      end
+
+      def set_context(page)
+        @context = page
+      end
+      attr_reader :context
+  
+      def search(text)
+       @browser.text.include? text 
+      end
+
+      def close
+        @browser.close
+        @headless.destroy
+      end
+
+      def get_picture
+        @browser.driver.save_screenshot 'screenshot.png'
+      end
+      def get_title
+        @browser.title
+      end
+      def get_response_time
+        #pending
+      end
+
+        def method_missing(method_name, *arguments)
+          @context.send(method_name, *arguments)
+        end
+
+    end
+  end
+end
+
