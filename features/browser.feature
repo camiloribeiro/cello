790998336fae4734eb04e03d84bdbd38b4ffd858
cribeiro <Thoughtworks@tw008317.home>
2012-06-29 02:43:25 -0300

    adding tets for browser

---
+++ features/browser.feature
@@ -0,0 +1,25 @@
+Feature: Browser
+  As a developer
+  I want use a browser in different contexts
+
+  Scenario: Get a context
+    Given I have a browser with no context (blank page)
+    When I ask for the context inputs
+    And and I ask to visit the page
+    Then I should see the page inputs
+
+  Scenario: Fill a textfield on a context
+    Given I am in the inputs context
+    When I ask for fill the textfield
+    Then I should see the filled textfield
+    And I should be able to close the browser
+
+  Scenario: Pass by two different contexts
+    Given I am in the inputs context
+    When I ask for fill the textfield
+    Then I should see the filled textfield
+    When I click on the simple page link 
+    And I ask to use the simple page context
+    Then I should fail when try access the old textfield
+    And I should be able to verify the text on the new textfield
+    And I should be able to close the browser
---
+++ features/step_definitions/browser.rb
@@ -0,0 +1,55 @@
+Dir[File.dirname(__FILE__) + "/../../pages/*.rb"].each do |file| 
+  require file 
+end
+
+Given /^I have a browser with no context \(blank page\)$/ do
+  @browser = StaticPages::Site::Firefox.new
+end
+
+When /^I ask for the context inputs$/ do
+  @browser.set_context StaticPages::Site::InputPage.new(@browser)
+end
+
+When /^and I ask to visit the page$/ do
+  @browser.visit
+end
+
+Then /^I should see the page inputs$/ do
+  @browser.get_title.should == "Inputs page"
+  @browser.close
+end
+
+Given /^I am in the inputs context$/ do
+  @browser = StaticPages::Site::Firefox.new
+  @browser.set_context StaticPages::Site::InputPage.new(@browser)
+  @browser.visit
+  @browser.get_title.should == "Inputs page"
+end
+
+When /^I ask for fill the textfield$/ do
+  @browser.text_field_fill_with("It Works!")
+end
+
+Then /^I should see the filled textfield$/ do
+  @browser.text_field_get_text.should == "It Works!"
+end
+
+Then /^I should be able to close the browser$/ do
+  @browser.close
+end
+
+When /^I click on the simple page link$/ do
+  @browser.link_click
+end
+
+When /^I ask to use the simple page context$/ do
+  @browser.set_context StaticPages::Site::ResponsePage.new(@browser)
+end
+
+Then /^I should fail when try access the old textfield$/ do
+ expect { @browser.text_field_get_text}.should raise_error(NoMethodError)
+end
+
+Then /^I should be able to verify the text on the new textfield$/ do
+  @browser.text_get_text.should == "Worked Again!"
+end
