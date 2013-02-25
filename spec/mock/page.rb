401560c18a10eb49f593724515247346736d1a03
Camilo Ribeiro <cribeiro@cribeiro.(none)>
2013-02-25 03:17:46 -0300

    add mock page to unit test

---
+++ spec/mock/page.rb
@@ -0,0 +1,25 @@
+require "cello"
+
+module Mock
+  module Site
+    class MockPage < Cello::Structure::Page
+      element :element,  :element, :id => 'elem1'
+      element :text_field,  :text_field, :id => 'text1'
+      element :text_fieldname,  :text_field, :name => 'text1'
+      element :text_fieldxpath,  :text_field, :xpah, '//*[@id="text1"]'
+      element :checkbox,  :checkbox, :id => 'check1'
+      element :textarea,  :textarea, :id => 'area1'
+      element :radios,  :radios, :name => 'items'
+      element :select,  :select, :id => 'select1'
+      element :link,  :link, :id => 'link1'
+      
+      def get_url
+        'file://' + File.dirname(__FILE__) + '/../site/inputs.html'
+      end
+
+      def initialize(teste)
+        super(teste)
+      end
+    end
+  end
+end
