657e9399edff0015cef017b36782454d59224b56
Camilo Ribeiro <cribeiro@cribeiro.(none)>
2013-02-25 09:55:59 -0300

    add unit tests for textarea

--- spec/textarea_spec.rb
+++ spec/textarea_spec.rb
@@ -1,24 +1,36 @@
-require File.join(File.dirname(__FILE__), '../lib/cello/structure/html_elements/textarea_helper.rb')
+require "mock/page"
 
 describe Cello::Structure::TextareaHelper do
   describe "define_extras_for_textarea" do
-    it "Veify if the textarea is enabled" do
+    before(:all) do
+      @page = Mock::Site::MockPage.new "foo"
     end
-    it "Clear the textarea" do
+    it "Veify if the textarea is enable method exists" do
+      (@page.methods.map.include? "textarea_is_enable?").should be_true
+    end
+    it "Clear the textarea method exists" do
+      (@page.methods.map.include? "textarea_clear").should be_true
     end
     it "Get the text from the textarea" do
+      (@page.methods.map.include? "textarea_get_text").should be_true
     end
     it "Fill the textarea with some specific text" do
+      (@page.methods.map.include? "textarea_fill_with").should be_true
     end
     it "Verify if the textarea does not contain some text" do
+      (@page.methods.map.include? "textarea_dont_contain").should be_true
     end
     it "Verify if the textarea contains some text" do
+      (@page.methods.map.include? "textarea_contains").should be_true
     end
     it "Verify if the containt of the textarea is exacly some text" do
+      (@page.methods.map.include? "textarea_text_is").should be_true
     end
     it "Verify if the textarea is empty" do
+      (@page.methods.map.include? "textarea_is_empty?").should be_true
     end
     it "Get the size of the containt of the textarea" do
+      (@page.methods.map.include? "textarea_text_size").should be_true
     end
   end
 end
