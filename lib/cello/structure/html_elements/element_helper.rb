5801354baaebda83e648afc4db1e10d7ac783857
Camilo Ribeiro <camilo.ribeiro@klarna.com>
2013-11-20 20:22:11 +0100

    adding logger to cello

--- lib/cello/structure/html_elements/element_helper.rb
+++ lib/cello/structure/html_elements/element_helper.rb
@@ -5,43 +5,65 @@ end
 module Cello
   module Structure
     module ElementHelper
-
+      
       private
 
-      def element(name, type, *args)
+      def element(name, type, *args)        
         class_eval do
           define_method name do
             engine.browser.send(type, *args)
           end
 
+          #define_extras(name, type)
           define_extras(name, type)
         end
+
       end
 
       def define_extras(name, type)
+        include LogHelper
+        
+        define_method "#{name}_exit_wait?" do
+            send(name).visible? 
+        end
+
+        define_method "#{name}_is_enable?" do
+          logger(name, __method__, type) {
+            send(name).enabled? 
+          }
+        end
         define_method "#{name}_is_real?" do
-          send(name).exists?
+          logger(name, __method__, type, "Foo") {
+            send(name).exists?        
+          }
         end
         define_method "#{name}_click" do
-          send(name).click
+          logger(name, __method__, type) {
+#            require "pry"; binding.pry
+            send(name).click
+          }
         end
         define_method "#{name}_is_visible?" do
-          send(name).visible?
-        end
-        define_method "#{name}_is_enable?" do
-          send(name).enabled?
+          logger(name, __method__, type) {
+            send(name).visible?
+          }
         end
         define_method "#{name}_right_click" do
-          send(name).right_click
+          logger(name, __method__, type) {
+            send(name).right_click
+          }
         end
 
         define_method "#{name}_get" do |att|
-          send(name).attribute_value(att)
+          logger(name, __method__, type) {
+            send(name).attribute_value(att)
+          }
         end
 
         method_name = "define_extras_for_#{type}"
         send(method_name, name) if respond_to? method_name
       end
+      
 
      include CheckboxHelper
      include TextfieldHelper
--- lib/cello/structure/html_elements/select_helper.rb
+++ lib/cello/structure/html_elements/select_helper.rb
@@ -2,21 +2,31 @@ module Cello
   module Structure
     module SelectHelper
       def define_extras_for_select(name)
+        include LogHelper
+
         define_method "#{name}_get_options" do
-          options = Array.new
-          send(name).options.each do |option|
-            options.push option.text
-          end
-          options
+          logger(name, __method__, :select) {
+            options = Array.new
+            send(name).options.each do |option|
+              options.push option.text
+            end
+            options
+          }
         end
         define_method "#{name}_select" do |option|
-          send(name).select option
+          logger(name, __method__, :select) {
+            send(name).select option
+          }
         end
         define_method "#{name}_is" do |option|
-          send(name).selected_options.last.text == option
+          logger(name, __method__, :select) {
+            send(name).selected_options.last.text == option
+          }
         end
         define_method "#{name}_selected" do
-          send(name).selected_options.last.text
+          logger(name, __method__, :select) {
+            send(name).selected_options.last.text
+          }
         end
       #  define_method "#{name}_clear" do
       #    send(name).clear
