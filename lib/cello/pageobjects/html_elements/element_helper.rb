a7f90c21ea2d6c5a4955ddfd3e0bc17cefbb1964
Camilo Ribeiro <camilo@camiloribeiro.com>
2015-04-23 20:49:32 +0200

    Turning off the logs. It will be back when complete

--- lib/cello/pageobjects/html_elements/element_helper.rb
+++ lib/cello/pageobjects/html_elements/element_helper.rb
@@ -20,60 +20,60 @@ module Cello
 
 
       def define_extras(name, type)
-        include LogHelper
-        
+        #include LogHelper
+
         define_method "#{name}_exit_wait?" do
-            send(name).visible? 
+          send(name).visible? 
         end
 
         define_method "#{name}_is_enable?" do
-          logger(name, __method__, type) {
-            send(name).enabled? 
-          }
+          #logger(name, __method__, type) {
+          send(name).enabled? 
+          #}
         end
         define_method "#{name}_is_real?" do
-          logger(name, __method__, type, "Foo") {
-            send(name).exists?        
-          }
+          #logger(name, __method__, type, "Foo") {
+          send(name).exists?        
+          #}
         end
         define_method "#{name}_click" do
-          logger(name, __method__, type) {
-            send(name).click
-          }
+          #logger(name, __method__, type) {
+          send(name).click
+          #}
         end
         define_method "#{name}_is_visible?" do
-          logger(name, __method__, type) {
-            send(name).visible?
-          }
+          #logger(name, __method__, type) {
+          send(name).visible?
+          #}
         end
         define_method "#{name}_right_click" do
-          logger(name, __method__, type) {
-            send(name).right_click
-          }
+          #logger(name, __method__, type) {
+          send(name).right_click
+          #}
         end
 
         define_method "#{name}_get" do |att|
-          logger(name, __method__, type) {
-            send(name).attribute_value(att)
-          }
+          #logger(name, __method__, type) {
+          send(name).attribute_value(att)
+          #}
         end
 
         method_name = "define_extras_for_#{type}"
         send(method_name, name) if respond_to? method_name
       end
-      
 
-     include CheckboxHelper
-     include TextfieldHelper
-     include TextareaHelper
-     include SelectHelper
-     include DivHelper
-     include RadioHelper
-     include LinkHelper
-     include ButtonHelper
-     include SpanHelper
-     include HiddenHelper
-     include IframeHelper
+
+      include CheckboxHelper
+      include TextfieldHelper
+      include TextareaHelper
+      include SelectHelper
+      include DivHelper
+      include RadioHelper
+      include LinkHelper
+      include ButtonHelper
+      include SpanHelper
+      include HiddenHelper
+      include IframeHelper
 
     end
   end
--- lib/cello/pageobjects/html_elements/select_helper.rb
+++ lib/cello/pageobjects/html_elements/select_helper.rb
@@ -5,28 +5,28 @@ module Cello
         include LogHelper
 
         define_method "#{name}_get_options" do
-          logger(name, __method__, :select) {
-            options = Array.new
-            send(name).options.each do |option|
-              options.push option.text
-            end
-            options
-          }
+          # logger(name, __method__, :select) {
+          options = Array.new
+          send(name).options.each do |option|
+            options.push option.text
+          end
+          options
+          #}
         end
         define_method "#{name}_select" do |option|
-          logger(name, __method__, :select) {
-            send(name).select option
-          }
+          #logger(name, __method__, :select) {
+          send(name).select option
+          #}
         end
         define_method "#{name}_is" do |option|
-          logger(name, __method__, :select) {
-            send(name).selected_options.last.text == option
-          }
+          #logger(name, __method__, :select) {
+          send(name).selected_options.last.text == option
+          #}
         end
         define_method "#{name}_selected" do
-          logger(name, __method__, :select) {
-            send(name).selected_options.last.text
-          }
+          #logger(name, __method__, :select) {
+          send(name).selected_options.last.text
+          #}
         end
       #  define_method "#{name}_clear" do
       #    send(name).clear
